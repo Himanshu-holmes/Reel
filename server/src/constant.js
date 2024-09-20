4ab85419c3ca3bc8c352fd5f81bb9c7bfc920263
heeman <himanshuholmes@gmail.com>
2024-09-20 23:13:12 +0530

    db setup

--- server/src/constant.js
+++ server/src/constant.js
@@ -6,9 +6,15 @@ const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
  const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 
+const DB_NAME = 'reel'
+const DB_URI = process.env.MONGODB_URI
+let count = 0;
 module.exports = {
     PORT,
     CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET
+    CLOUDINARY_API_SECRET,
+    DB_NAME,
+    DB_URI,
+    count
     };
